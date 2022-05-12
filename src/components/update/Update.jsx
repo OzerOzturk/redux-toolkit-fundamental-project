3e83904b9610d568bdef95fb403c38c7bedf54b1
OzerOzturk <ozerozturk1636@gmail.com>
2022-05-12 14:38:22 +0300

    add apiCalls.js

--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -4,21 +4,22 @@ import "./update.css";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { remove, update } from "../../redux/userSlice";
+import { updateUser } from "../../redux/apiCalls";
 
 export default function Update() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const user = useSelector((state) => state.user);
+  const { userInfo, pending, error } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   const handleUpdate = (e) => {
     e.preventDefault();
-    dispatch(update({ name, email }));
+    updateUser({ name, email }, dispatch);
   };
 
   const handleDelete = (e) => {
     e.preventDefault();
-    dispatch(remove());
+    /* dispatch(remove()); */
   };
 
   return (
@@ -47,7 +48,7 @@ export default function Update() {
               <input
                 className="formInput"
                 type="text"
-                placeholder={user.name}
+                placeholder={userInfo.name}
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
@@ -56,7 +57,7 @@ export default function Update() {
               <input
                 className="formInput"
                 type="text"
-                placeholder={user.email}
+                placeholder={userInfo.email}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
@@ -64,9 +65,17 @@ export default function Update() {
               <label>Password</label>
               <input className="formInput" type="password" />
             </div>
-            <button className="updateButton" onClick={handleUpdate}>
+            <button
+              disabled={pending}
+              className="updateButton"
+              onClick={handleUpdate}
+            >
               Update
             </button>
+            {error && <span className="error">Something went wrong!</span>}
+            {pending === false && (
+              <span className="success">Account has been updated!</span>
+            )}
           </form>
         </div>
       </div>
